f7a521d5952ddf486dc24e1a2c76df564f47686f
naderHaseeb <naderhaseeb@gmail.com>
2026-08-05 15:28:42 +0300

    Update helper.py

--- helper.py
+++ helper.py
@@ -162,7 +162,7 @@ def generate_smart_recipe(ingredients, diet, api_key):
 
     try:
         response = client.models.generate_content(
-            model="gemini-2.5-flash",
+            model="gemini-2.5-flash-lite",
             contents=prompt
         )
 
